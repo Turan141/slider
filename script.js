e1ae030db381858176204a6ae0693a35d132b3fb
Turan <90398677+Turan141@users.noreply.github.com>
2022-02-19 16:29:51 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    let prevBtn = document.querrySelector("#prev")
+    let nextBtn = document.querrySelector("#next")
+    let sliderImg = document.querrySelector(".line")
+
+
+    nextBtn.addEventListener()
+
+    
